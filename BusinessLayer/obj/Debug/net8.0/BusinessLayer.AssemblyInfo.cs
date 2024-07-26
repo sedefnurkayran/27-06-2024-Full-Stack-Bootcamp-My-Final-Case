8fb90d5e872fd624dd604baf80e9c7e60b15cb52
Sedefnur Kayran <109733884+sedefnurkayran@users.noreply.github.com>
2024-07-26 18:09:22 +0300

    Authentication problem solved

--- BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f6885fc587a87bdb56e59173591bdec23419a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2c363d0dee236cf332ff002345feb3824f950b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
