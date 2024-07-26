d62fc8a4fdbf4ce6dbc02c2df0ef02297918a636
Sedefnur Kayran <109733884+sedefnurkayran@users.noreply.github.com>
2024-07-26 09:35:22 +0300

    DAL migrations folder removedd

--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f6885fc587a87bdb56e59173591bdec23419a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e21424c0d8351454e8fc3cb4e3d083fe5ab5f4ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
